39d04ce11155e8f3d1a2ed25eb595696f0490f03
paronne <paronne@servoy.com>
2017-08-24 11:36:19 +0300

    hardcoded workspace name

--- jenkins-build/kubernetes/pre-ant.sh
+++ jenkins-build/kubernetes/pre-ant.sh
@@ -5,7 +5,7 @@
 #
 
 echo "Spinning-up a container with Servoy $SVY_VERSION"
-/var/lib/jenkins/workspace/svyMicroSamples/jenkins-build/kubernetes/api-calls/dev-app-pod.sh -a developerapp -n demo -l developerapp -c developerapp -i servoy/alpine-appserver-developer-nfs:$SVY_VERSION-jar-included
+/var/lib/jenkins/workspace/svyGroupingGrid/jenkins-build/kubernetes/api-calls/dev-app-pod.sh -a developerapp -n demo -l developerapp -c developerapp -i servoy/alpine-appserver-developer-nfs:$SVY_VERSION-jar-included
 sleep 60
 echo "Scanning for dev-app Pod IP.."
 devapppod=$(sudo kubectl get pods -n demo -o wide | grep developerapp | awk '{print $6}')
