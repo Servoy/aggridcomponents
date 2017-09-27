67e52b86704ea06691784d37e33687711d81b711
paronne <paronne@servoy.com>
2017-09-27 17:56:54 +0300

    Remove jenkins-build folder

--- jenkins-build/e2e/env.js
+++
@@ -1,5 +0,0 @@
-var configure = function () {
-  this.setDefaultTimeout(60*1000);
-};
-
-module.exports = configure;
--- jenkins-build/e2e/lib/cucumberLoader.js
+++
@@ -1,7 +0,0 @@
-module.exports.load = function() {
-  if (process.env.MULTIDEP_CUCUMBER_VERSION) {
-    return require('multidep')('test/multidep.js')('cucumber', process.env.MULTIDEP_CUCUMBER_VERSION);
-  } else {
-    return require('cucumber');
-  }
-};
