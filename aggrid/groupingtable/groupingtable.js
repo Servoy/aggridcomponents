d6240d3b8f7910e3f5190ac860aa910e9cd9077e
lvostinar <lvostinar@servoy.com>
2018-04-04 14:07:46 +0300

    SVY-12288 AG Grid: Issue with column that has no data provider set

--- aggrid/groupingtable/groupingtable.js
+++ aggrid/groupingtable/groupingtable.js
@@ -1246,7 +1246,7 @@ angular.module('aggridGroupingtable', ['servoy', 'aggridenterpriselicensekey']).
 								var header = $scope.model.columns[i];
 								var field = getColumnID(header, i);
 
-								var value = header.dataprovider[index];
+								var value = header.dataprovider ? header.dataprovider[index] : null;
 								r[field] = value;
 							}
 							return r;
