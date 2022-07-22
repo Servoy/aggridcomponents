5c65614a0dd4c3eb6ef99773dcadd052d20f9306
Gabi Boros <gboros@servoy.com>
2022-07-22 15:56:35 +0300

    SVY-17244 use addFoundSetFilterParam api if available

--- aggrid/groupingtable/groupingtable_server.js
+++ aggrid/groupingtable/groupingtable_server.js
@@ -211,7 +211,7 @@ $scope.filterMyFoundset = function(sFilterModel, foundset) {
 			myFoundset = foundset;
 		} else {
 			myFoundset = $scope.model.myFoundset.foundset;
-			if(myFoundset.getRelationName && myFoundset.getRelationName()) {
+			if(myFoundset.getRelationName && myFoundset.getRelationName() && !servoyApi.addFoundSetFilterParam) {
 				myFoundset = myFoundset.unrelate();
 				$scope.model._internalUnrelatedMyFoundsetForFilter = {
 					foundset: myFoundset
@@ -558,7 +558,7 @@ function filterFoundset(foundset, sFilterModel) {
 	}
 
 	if(isFilterSet) {
-		foundset.addFoundSetFilterParam(query, "ag-groupingtable");
+		servoyApi.addFoundSetFilterParam ? servoyApi.addFoundSetFilterParam(foundset, query, "ag-groupingtable") : foundset.addFoundSetFilterParam(query, "ag-groupingtable");
 		shouldReloadWithFilters = true;
 	}
 
