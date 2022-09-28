1adcba6027506cf1294b065a1ece5f6eb278ddf6
Gabi Boros <gboros@servoy.com>
2022-09-28 14:11:53 +0300

    SVY-17270 Drag & Drop in DataGrid

--- aggrid/projects/nggrids/src/datagrid/datagrid.service.ts
+++ aggrid/projects/nggrids/src/datagrid/datagrid.service.ts
@@ -1,4 +1,6 @@
+import { RowDropZoneParams } from '@ag-grid-community/core';
 import { Injectable } from '@angular/core';
+import { DataGrid } from './datagrid';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +14,56 @@ export class DatagridService {
     public mainMenuItemsConfig: any;
     public arrowsUpDownMoveWhenEditing: string;
     public editNextCellOnEnter: boolean;
+
+    private gridNameToRowDropZoneInfo: Map<string, RowDropZoneInfo> = new Map();
+
+    addRowDropZone(dataGrid: DataGrid, rowDropZone: RowDropZoneParams) {
+      if(rowDropZone && dataGrid.rowDropZoneFor && dataGrid.rowDropZoneFor.length) {
+        dataGrid.rowDropZoneFor.forEach(forGridName => {
+          if(this.gridNameToRowDropZoneInfo.has(forGridName)) {
+            this.gridNameToRowDropZoneInfo.get(forGridName).dataGrid.agGrid.api.addRowDropZone(rowDropZone);
+          }
+        });
+      }
+
+      this.gridNameToRowDropZoneInfo.forEach(rowDropZoneInfo => {
+        if(rowDropZoneInfo.rowDropZone && rowDropZoneInfo.dataGrid.rowDropZoneFor && rowDropZoneInfo.dataGrid.rowDropZoneFor.length) {
+          rowDropZoneInfo.dataGrid.rowDropZoneFor.forEach(forGridName => {
+            if(dataGrid.name === forGridName) {
+              dataGrid.agGrid.api.addRowDropZone(rowDropZoneInfo.rowDropZone);
+            }
+          });
+        }
+      });
+      this.gridNameToRowDropZoneInfo.set(dataGrid.name,  new RowDropZoneInfo(dataGrid, rowDropZone));
+    }
+
+    removeRowDropZone(dataGrid: DataGrid) {
+      if(this.gridNameToRowDropZoneInfo.has(dataGrid.name)) {
+        const rowDropZoneInfo = this.gridNameToRowDropZoneInfo.get(dataGrid.name);
+        if(rowDropZoneInfo.rowDropZone && rowDropZoneInfo.dataGrid.rowDropZoneFor && rowDropZoneInfo.dataGrid.rowDropZoneFor.length) {
+          dataGrid.rowDropZoneFor.forEach(forGridName => {
+            if(this.gridNameToRowDropZoneInfo.has(forGridName)) {
+              this.gridNameToRowDropZoneInfo.get(forGridName).dataGrid.agGrid.api.removeRowDropZone(rowDropZoneInfo.rowDropZone);
+            }
+          });
+        }
+        this.gridNameToRowDropZoneInfo.delete(dataGrid.name)
+      }
+
+      this.gridNameToRowDropZoneInfo.forEach(rowDropZoneInfo => {
+        if(rowDropZoneInfo.rowDropZone && rowDropZoneInfo.dataGrid.rowDropZoneFor && rowDropZoneInfo.dataGrid.rowDropZoneFor.length) {
+          rowDropZoneInfo.dataGrid.rowDropZoneFor.forEach(forGridName => {
+            if(dataGrid.name === forGridName) {
+              dataGrid.agGrid.api.removeRowDropZone(rowDropZoneInfo.rowDropZone);
+            }
+          });
+        }
+      });
+    }
+}
+
+class RowDropZoneInfo {
+  constructor(public dataGrid: DataGrid, public rowDropZone: RowDropZoneParams) {
+  }
 }
