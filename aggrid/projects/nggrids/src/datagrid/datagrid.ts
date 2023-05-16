bb607369e174c180dcd9aa1d70290f7da93023f0
Gabi Boros <gboros@servoy.com>
2023-05-16 12:09:28 +0300

    SVY-18174 Related grid stops displaying records after adding columns

--- aggrid/projects/nggrids/src/datagrid/datagrid.ts
+++ aggrid/projects/nggrids/src/datagrid/datagrid.ts
@@ -4322,6 +4322,9 @@ class FoundsetServer {
                         });
                     } else { // set the grid sorting if foundset sort changed from the grid initialization (like doing foundset sort on form's onShow)
                         this.dataGrid.applySortModel(this.dataGrid.getSortModel());
+                        // aggrid's callback must be called else the current request will be counting as 'pending' and
+                        // adding up for max concurrent requests, blocking any succesive requests
+                        callback([]);
                         this.dataGrid.refreshAgGridServerSide();
                     }
                 } else {
