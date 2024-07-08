ea11dcde3d6697308f8eac84a9193a71d1d6f8fc
Gabi Boros <gboros@servoy.com>
2024-07-08 15:40:29 +0300

    SVY-19407 Grid with related foundset loads wrong data when parent foundset is empty

--- aggrid/projects/nggrids/src/datagrid/datagrid.ts
+++ aggrid/projects/nggrids/src/datagrid/datagrid.ts
@@ -3814,6 +3814,9 @@ export class DataGrid extends NGGridDirective {
 
         if(!this.isTableGrouped() && changeEvent.serverFoundsetSizeChanged) {
             this.agGrid.api.setRowCount(changeEvent.serverFoundsetSizeChanged.newValue);
+            if(changeEvent.serverFoundsetSizeChanged.newValue === 0) {
+                return;
+            }
         }
 
         if(changeEvent.multiSelectChanged) {
