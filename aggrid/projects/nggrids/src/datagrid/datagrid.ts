46bc8ed9f9c3292e1c08d82ef76a6c57efaa9a29
Gabi Boros <gboros@servoy.com>
2024-08-20 11:32:20 +0300

    fix this reference on record update

--- aggrid/projects/nggrids/src/datagrid/datagrid.ts
+++ aggrid/projects/nggrids/src/datagrid/datagrid.ts
@@ -2315,7 +2315,7 @@ export class DataGrid extends NGGridDirective {
                                     rowIndex,
                                     colKey: colId
                                 });
-                                this.setTimeout(() => {
+                                _this.setTimeout(() => {
                                     _this.selectionEvent = null;
                                     _this.agGrid.api.forEachNode( (node: any) => {
                                         if (node.rowIndex === rowIndex) {
@@ -2337,9 +2337,9 @@ export class DataGrid extends NGGridDirective {
                         }
                         _this.onColumnDataChangePromise = null;
                     }).catch((e: any) => {
-                        this.log.error(e);
-                        this.invalidCellDataIndex.rowIndex = -1;
-                        this.invalidCellDataIndex.colKey = '';
+                        _this.log.error(e);
+                        _this.invalidCellDataIndex.rowIndex = -1;
+                        _this.invalidCellDataIndex.colKey = '';
                         _this.onColumnDataChangePromise = null;
                     });
                 }
