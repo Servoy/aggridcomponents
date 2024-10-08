a8374adf22aed1f62cd1f5385da9a07261dc0d16
Gabi Boros <gboros@servoy.com>
2024-10-08 12:49:12 +0300

    SVY-19618 aggrid-groupingtable: When filtering a DATE column and range, the 'until' time value contains 00:00:00

--- aggrid/groupingtable/groupingtable_server.js
+++ aggrid/groupingtable/groupingtable_server.js
@@ -551,9 +551,15 @@ function getFilterWhereClauseForDataprovider(query, filter, dp, columnFormat) {
 				break;
 			case "inRange":
 				op = "between";
+				var dateFromSplit = value.split("-");
+				var dateFromD = new Date(dateFromSplit[0], dateFromSplit[1] - 1, dateFromSplit[2]);
+				var dateToSplit = getConvertedDate(filter["dateTo"].split(" ")[0], filter["dateToMs"], columnFormat).split("-");
+				var dateToD = new Date(dateToSplit[0], dateToSplit[1] - 1, dateToSplit[2]);
+				dateToD.setDate(dateToD.getDate() + 1);
+				dateToD.setTime(dateToD.getTime() - 1);
 				var valueA = new Array();
-				valueA.push(value);
-				valueA.push(getConvertedDate(filter["dateTo"].split(" ")[0], filter["dateToMs"], columnFormat));
+				valueA.push(dateFromD);
+				valueA.push(dateToD);
 				value = valueA;
 				break;
 			case "notBlank":
