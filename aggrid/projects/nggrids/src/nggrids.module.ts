588b458a6d263da911ee99372d9fe944fb0838b4
Gabi Boros <gboros@servoy.com>
2025-03-11 13:11:53 +0200

    SVY-20074 Ag-grid throws module error: RowStyleModule is not registered

--- aggrid/projects/nggrids/src/nggrids.module.ts
+++ aggrid/projects/nggrids/src/nggrids.module.ts
@@ -9,7 +9,7 @@ import { SelectEditor } from './editors/selecteditor';
 import { TextEditor } from './editors/texteditor';
 import { TypeaheadEditor } from './editors/typeaheadeditor';
 import { AgGridModule } from 'ag-grid-angular';
-import { ClientSideRowModelModule, CellStyleModule, ColumnApiModule, ColumnAutoSizeModule, DragAndDropModule, EventApiModule, LocaleModule, ModuleRegistry, NumberFilterModule, PinnedRowModule, RowApiModule, RowAutoHeightModule, ScrollApiModule, TooltipModule, ValidationModule, TextFilterModule, CustomEditorModule, DateFilterModule, TextEditorModule, RenderApiModule } from 'ag-grid-community';
+import { ClientSideRowModelModule, CellStyleModule, ColumnApiModule, ColumnAutoSizeModule, DragAndDropModule, EventApiModule, LocaleModule, ModuleRegistry, NumberFilterModule, PinnedRowModule, RowApiModule, RowAutoHeightModule, ScrollApiModule, TooltipModule, ValidationModule, TextFilterModule, CustomEditorModule, DateFilterModule, TextEditorModule, RenderApiModule, RowStyleModule } from 'ag-grid-community';
 import { ClipboardModule, RowGroupingModule, SideBarModule, MenuModule, ExcelExportModule, CellSelectionModule, FiltersToolPanelModule, ColumnsToolPanelModule, RowGroupingPanelModule, ServerSideRowModelApiModule, ServerSideRowModelModule } from 'ag-grid-enterprise';
 import { ValuelistFilter } from './filters/valuelistfilter';
 import { RadioFilter } from './filters/radiofilter';
@@ -87,6 +87,7 @@ export class NGGridsModule {
             TextEditorModule,
             CustomEditorModule,
             ValidationModule,
-            RenderApiModule]);
+            RenderApiModule,
+            RowStyleModule]);
     }
 }
