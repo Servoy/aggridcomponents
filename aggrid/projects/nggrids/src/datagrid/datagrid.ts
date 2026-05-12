42b772975c1f63c9932400b76250410db7106d01
Gabi Boros <gboros@servoy.com>
2026-05-12 17:27:04 +0300

    SVY-21065 Data Grid sort icon disappears when onSort is overridden

--- aggrid/projects/nggrids/src/datagrid/datagrid.ts
+++ aggrid/projects/nggrids/src/datagrid/datagrid.ts
@@ -1361,7 +1361,7 @@ export class DataGrid extends NGGridDirective {
     initRootFoundset() {
 
         this.foundset = new FoundsetManager(this, this.myFoundset, 'root', true);
-		if (this.onSort()) {
+		if (this.onSort) {
 			this.applySortModel(this.getSortModel());
 		}
         const foundsetServer = new FoundsetServer(this, []);
